8f6b2f637ad17f3c77457b3ef8660916e3b9e005
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2021-10-02 19:41:45 -0400

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
 });
 
+router.get('/submit?', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+
 module.exports = router;
