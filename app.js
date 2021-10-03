0f20fbc9ba21946b623e174f7e4a3d7defefdf30
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2021-10-02 22:32:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
+//Declaring imports
+//External Modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Local Modules
 var indexRouter = require('./routes/index');
 var contactRouter = require('./routes/contact');
 
+//Instantiating the Express module
 var app = express();
 
-// view engine setup
+//Configuring Express
+//set path for views 
 app.set('views', path.join(__dirname, 'views'));
+//set ejs as the view engine
 app.set('view engine', 'ejs');
 
+//Creating middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Setting static folders
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
+//Setting HTTP request handlers
 app.use('/', indexRouter);
 app.use('/contact', contactRouter);
 
-// catch 404 and forward to error handler
+//Predefined callback function in case of error 
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+//Predefined error handling 
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
